a24ad496f232512c8ba626544cb2ef36df8100b8
Zheng Zikang <zhengzikangzzz@gmail.com>
2020-07-28 00:54:32 +0800

    test other user app calls by using Jest matchers

--- test/user.test.js
+++ test/user.test.js
@@ -1,11 +1,20 @@
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const app = require("../src/app");
 const User = require("../src/models/user");
 
+const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
+  _id: userOneId,
   name: "test",
   email: "test@email.com",
   password: "1234567",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
+    },
+  ],
 };
 
 // run before each test
@@ -18,9 +27,8 @@ beforeEach(async () => {
 //   console.log("afterEach");
 // });
 
-// create new user
-test("Should signupa new user", async () => {
-  await request(app)
+test("Should signup a new user", async () => {
+  const response = await request(app)
     .post("/users")
     .send({
       name: "zzk",
@@ -28,20 +36,34 @@ test("Should signupa new user", async () => {
       password: "1234567",
     })
     .expect(201);
+
+  // Assertions that the database was changed correctly
+  const user = await User.findById(response.body.user._id);
+  expect(user).not.toBeNull();
+
+  // Assertions about the response
+  expect(response.body).toMatchObject({
+    user: {
+      name: "zzk",
+    },
+  });
+
+  expect(user.email).not.toBe("zzk2@email.com");
 });
 
-// login user
 test("Should login a new user", async () => {
-  await request(app)
+  const response = await request(app)
     .post("/users/login")
     .send({
       email: userOne.email,
       password: userOne.password,
     })
     .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(response.body.token).toBe(user.tokens[1].token);
 });
 
-// login user failure
 test("Should login failure", async () => {
   await request(app)
     .post("/users/login")
@@ -50,4 +72,32 @@ test("Should login failure", async () => {
       password: userOne.password,
     })
     .expect(400);
+});
+
+test("Should get profile for user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should not get profile for unauthenticated user", async () => {
+  await request(app).get("/users/me").send().expect(401);
+});
+
+test("Should delete user account", async () => {
+  await request(app)
+    .delete("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  // Assertions that the user was removed
+  const user = await User.findById(userOneId);
+  expect(user).toBeNull();
+});
+
+test("Should not delete account for unauthenticated user", async () => {
+  await request(app).delete("/users/me").send().expect(401);
 });
